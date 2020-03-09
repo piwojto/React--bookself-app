3d2ebf2edf16d820e6a2564c393bd000007fd865
hydroporto <hydroporto@gmail.com>
2020-03-09 14:19:55 +0100

    Updates

---
+++ precache-manifest.c2d8b0f30aa0dc14727da5850d01ad3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803bda72ec1c313b8835e24075fc4973",
+    "url": "/React--bookself-app/index.html"
+  },
+  {
+    "revision": "2543c629cbcb6a796bf4",
+    "url": "/React--bookself-app/static/css/main.50279c94.chunk.css"
+  },
+  {
+    "revision": "0eef4c31d1997bcdbce5",
+    "url": "/React--bookself-app/static/js/2.f17fa8c9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React--bookself-app/static/js/2.f17fa8c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2543c629cbcb6a796bf4",
+    "url": "/React--bookself-app/static/js/main.db90b79a.chunk.js"
+  },
+  {
+    "revision": "2b504a19f0e310fc78b5",
+    "url": "/React--bookself-app/static/js/runtime-main.47651fac.js"
+  },
+  {
+    "revision": "ee40a1b1105c135937759a29f5d83317",
+    "url": "/React--bookself-app/static/media/books.ee40a1b1.jpg"
+  }
+]);
